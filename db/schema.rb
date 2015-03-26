17a7d72f93b2b663e02715b6d4fa7494aa8a7e45
Ivica Lakatos <lakatos.ivica@gmail.com>
2015-03-26 11:50:04 +0100

    Add associotion between Character and User

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326100638) do
+ActiveRecord::Schema.define(version: 20150326104455) do
 
   create_table "characters", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "characters", ["user_id"], name: "index_characters_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
