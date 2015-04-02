1b35f8cefae3c88abac15a8a87fc1bd1d582630b
Ivica Lakatos <lakatos.ivica@gmail.com>
2015-04-02 22:34:02 +0100

    Add few more rspec tests.

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
-	describe Character do
-	  it "has a name" do
+describe Character do
+
+    it "has a name" do
 	  character = Character.new(name: 'Warrior')
 	  expect(character).to be_valid
 	end
+
+	it "is invalid without a name" do
+	  character = Character.new(name: nil)
+	  character.valid?
+	  expect(character.errors[:name]).to include("can't be blank")
+	 end
+
+	it "has a avatar" do
+      character = Character.new(name: 'Warrior', 
+  	  avatar: 'warrior.jpg')
+      expect(character).to be_valid
+    end
 end
