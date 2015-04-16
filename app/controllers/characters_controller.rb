0d76b6d128abe33449aa179594c40a6b608d152f
Ivica Lakatos <lakatos.ivica@gmail.com>
2015-04-16 18:23:17 +0100

    Improve character controller.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,68 +1,47 @@
 class CharactersController < ApplicationController
-  before_action :all_tasks, only: [:index, :create, :destroy]
+  before_action :all_characters, only: [:index, :create, :destroy]
   before_action :set_character, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!
   before_action :authorized_user, only: [:show, :edit, :update, :destroy]
-  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_character
 
-  # GET /characters
-  # GET /characters.json
-  # def index
-  # end
+  def index
+  end
 
-  # GET /characters/1
-  # GET /characters/1.json
   def show
   end
 
-  # GET /characters/new
   def new
     @character = current_user.characters.build
   end
 
-  # GET /characters/1/edit
   def edit
   end
 
-  # POST /characters
-  # POST /characters.json
   def create
     @character = current_user.characters.build(character_params)
 
     respond_to do |format|
       if @character.save
-        format.html { redirect_to @character, notice: 'Character was successfully created.' }
         format.js
-        format.json { render :show, status: :created, location: @character }
-      else
-        format.html { render :new }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /characters/1
-  # PATCH/PUT /characters/1.json
   def update
     respond_to do |format|
       if @character.update(character_params)
         format.html { redirect_to @character, notice: 'Character was successfully updated.' }
-        format.json { render :show, status: :ok, location: @character }
       else
         format.html { render :edit }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /characters/1
-  # DELETE /characters/1.json
   def destroy
     @character.destroy
     respond_to do |format|
-      format.html { redirect_to characters_url, notice: 'Character was successfully destroyed.' }
       format.js
-      format.json { head :no_content }
     end
   end
 
@@ -73,21 +52,20 @@ class CharactersController < ApplicationController
       redirect_to characters_path, notice: "Not authorized to edit this character!" if @character.nil?
     end
 
-    def all_tasks
-      @characters = Character.where(user_id: current_user).page(params[:page]).per(4)
+    def all_characters
+      @characters = current_user.characters.order("created_at DESC").page(params[:page]).per(4)
     end
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_character
       @character = Character.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def character_params
       params.require(:character).permit(:name, :avatar)
     end
 
-     def invalid_cart
-      logger.error "Attempt to access invalid cart #{params[:id]}"
-      redirect_to root_path, notice: 'Invalid cart!'
+     def invalid_character
+      logger.error "Attempt to access invalid character #{params[:id]}"
+      redirect_to root_path, notice: 'Invalid character!'
     end
 end
