7da432184a289a3d4de7e14fe4ba63f9388b6d15
Ivica Lakatos <lakatos.ivica@gmail.com>
2015-04-16 19:33:18 +0100

    Added validations for images.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,6 @@
 class Character < ActiveRecord::Base
-	validates :name, presence: true
+	validates :name, presence: true, length: {maximum: 50}
+	validates :avatar, presence: true
 	belongs_to :user
 	mount_uploader :avatar, AvatarUploader
     has_many :skills, dependent: :destroy
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,7 @@
 class Skill < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 30}
   validates :value, presence: true	
+  validates :icon, presence: true
   belongs_to :character
   mount_uploader :icon, IconUploader
 end
