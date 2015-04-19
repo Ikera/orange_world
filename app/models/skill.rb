2958c3f4f654081e8942b180603aa1b8cb528121
Ivica Lakatos <lakatos.ivica@gmail.com>
2015-04-19 18:33:33 +0100

    Add numericality for skill model and test it with rspec.

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,6 @@
 class Skill < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 30}
-  validates :value, presence: true	
+  validates :value, presence: true, numericality: {:greater_than => 9, :less_than => 101}	
   validates :icon, presence: true
   belongs_to :character
   mount_uploader :icon, IconUploader
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -10,4 +10,7 @@ describe Skill do
 	it { should validate_presence_of(:value)}
 	it { should validate_presence_of(:icon)}
 	it { should belong_to(:character) }
+	it { should_not allow_value('6').for(:value) }
+	it { should_not allow_value('101').for(:value) }
+	it { should allow_value('10').for(:value) }
 end
