ffad88dea32321eff5fcb2d664425692dbf99b42
Ivica Lakatos <lakatos.ivica@gmail.com>
2015-05-03 00:22:02 +0200

    Added admins controllers and admins views.

---
+++ app/controllers/admins/characters_controller.rb
@@ -0,0 +1,23 @@
+class Admins::CharactersController < ApplicationController
+  before_action :set_character, only: [:show, :destroy]
+  before_action :authenticate_admin!
+
+	def show 
+	  @skills = @character.skills
+	end
+
+	def destroy
+    @character.destroy
+    redirect_to admins_user_url([@character.user]), notice: 'Character was successfully destroyed.'
+	end
+    
+  private
+
+  def set_character
+    @character = Character.find(params[:id])
+  end
+
+  def char_params
+    params.require(:char).permit(:name, :avatar, :char_id)
+  end	
+end
---
+++ app/controllers/admins/users_controller.rb
@@ -0,0 +1,31 @@
+class Admins::UsersController < ApplicationController
+  before_action :all_users, only: [:index, :destroy]
+  before_action :set_user, only: [:show, :destroy]
+  before_action :authenticate_admin!
+
+  def index
+  end
+
+  def show  
+    @characters = @user.characters
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admins_users_url, notice: 'User was successfully destroyed.'  
+  end
+
+  private
+
+  def all_users
+    @users = User.all
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :user_id)
+  end	
+end
